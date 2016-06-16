5d718a0c02a5b7f5f05f3c838e4e28759e9c5167
niwenbin <niwenbin_1989@163.com>
2016-06-16 17:05:29 +0800

    更新

--- app/school/school.js
+++ app/school/school.js
@@ -13,4 +13,32 @@ app.controller('SchoolCtrl', function($scope, $http) {
           '         </div>'
       };
     })
+    .directive("schoolMenu", function() {
+//自定义模块
+        return {
+            replace:true,
+            template :'<div class="school_menu">' +
+            '            <div class="school_menu_box">' +
+            '            <a class="sel">首页</a' +
+            '            ><a>学院介绍</a' +
+            '            ><a>培训课程</a' +
+            '            ><a>立即报名</a>' +
+            '            </div>' +
+            '            </div>'
+        };
+    })
+    .directive("schoolLoginMenu", function() {
+//自定义模块
+      return {
+          replace:true,
+          template :'<div class="school_login_menu">' +
+          '          <a class="tab_login">登录</a' +
+          '          ><a class="tab_sign">注册</a' +
+          '            ><a class="tab_phone">手机版</a' +
+          '            ><a class="tab_help">帮助中心</a' +
+          '            ><a class="tab_wei"><img src="../images/school_wei.png"></a' +
+          '            ><a class="tab_qq"><img src="../images/school_qq.png"></a>' +
+          '            </div>'
+      };
+    })
 
